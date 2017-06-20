23a47b2503fe15b82d425d2268f13cccb79f7566
Eric St. John <ericstj@microsoft.com>
2017-06-20 10:40:13 -0700

    Sample app with bindingRedirects

---
+++ corefx21116/app/Program.cs
@@ -0,0 +1,21 @@
+﻿using lib;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var streamReader = new StreamReader(Class1.OpenFile("data.txt")))
+            {
+                Console.WriteLine(streamReader.ReadToEnd());
+            }
+        }
+    }
+}
---
+++ corefx21116/lib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace lib
+{
+    public class Class1
+    {
+        public static FileStream OpenFile(string file)
+        {
+            return new FileStream(file, FileMode.Open);
+        }
+    }
+}

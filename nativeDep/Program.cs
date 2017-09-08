37ebc6b7bd845d17b0f66bb4e5fbe0cee028131a
Eric St. John <ericstj@microsoft.com>
2017-09-08 09:39:08 -0700

    Sample of app calling native dependency

---
+++ nativeDep/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            SNIInitialize(IntPtr.Zero);
+        }
+
+        [DllImport("sni.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern uint SNIInitialize([In] IntPtr pmo);
+    }
+}

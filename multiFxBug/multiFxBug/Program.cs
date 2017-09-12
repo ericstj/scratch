3e49a8061e266674dc0ee849869973ee1a9376d8
Eric St. John <ericstj@microsoft.com>
2017-09-12 09:59:21 -0700

    Add multiFxBug repro

---
+++ multiFxBug/multiFxBug/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace multiFxBug
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

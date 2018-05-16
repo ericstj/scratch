03e12a000c4e6a2b0de627beababa45fd9e55d85
Eric St. John <ericstj@microsoft.com>
2018-05-16 15:16:28 -0700

    Sample that shows how to build two EXEs to a single directory.

---
+++ roslynSample/vbc/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace vbc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

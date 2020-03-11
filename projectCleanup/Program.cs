59f0a9a1a61da02b07dabe9d9f0023ac6cecdf54
Eric StJohn <ericstj@microsoft.com>
2020-03-11 08:53:15 -0700

    Add sample project for MSBuild eval API

---
+++ projectCleanup/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+using System.Runtime.CompilerServices;
+using Microsoft.Build.Evaluation;
+using Microsoft.Build.Locator;
+
+namespace projectCleanup
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            MSBuildLocator.RegisterMSBuildPath(Environment.ExpandEnvironmentVariables(@"%ProgramFiles%\dotnet\sdk\3.1.100"));
+            UpdateFiles();
+        }
+
+        // This must be a non-inlined method run after RegisterMSBuildPath so that the loader is hooked
+        // before this method is JIT'ed attempting to load MSBuild assemblies.
+        [MethodImpl(MethodImplOptions.NoInlining)]
+        public static void UpdateFiles()
+        {   
+            foreach(var projectFile in Directory.EnumerateFiles(@"C:\src\dotnet\runtime\src\libraries\", "*.csproj", SearchOption.AllDirectories))
+            {
+                Console.WriteLine(projectFile);
+                Project project = new Project(projectFile);
+
+                var assemblyNameProperty = project.GetProperty("AssemblyName");
+
+                if (assemblyNameProperty != null &&
+                    !assemblyNameProperty.IsImported &&
+                    assemblyNameProperty.UnevaluatedValue == Path.GetFileNameWithoutExtension(projectFile))
+                {
+                    project.RemoveProperty(assemblyNameProperty);
+                }
+
+                if (project.IsDirty)
+                {
+                    project.Save();
+                }
+            }
+        }
+    }
+}
